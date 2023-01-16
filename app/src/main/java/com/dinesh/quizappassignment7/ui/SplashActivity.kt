2a529beaca35930af9e2c9085ca7ee5cd12dae55
DineshSarma1 <dineshsarma202@gmail.com>
2023-01-15 18:49:38 -0600

    Ui improved and animations are added

--- app/src/main/java/com/dinesh/quizappassignment7/ui/SplashActivity.kt
+++ app/src/main/java/com/dinesh/quizappassignment7/ui/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.dinesh.quizappassignment7.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,7 +15,7 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        val getStartedBtn = findViewById<Button>(R.id.getStartedBtn)
+        val getStartedBtn = findViewById<TextView>(R.id.getStartedBtn)
 
         getStartedBtn.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
--- app/src/main/java/com/dinesh/quizappassignment7/ui/result/ResultAnalysisAdapter.kt
+++ app/src/main/java/com/dinesh/quizappassignment7/ui/result/ResultAnalysisAdapter.kt
@@ -1,7 +1,11 @@
 package com.dinesh.quizappassignment7.ui.result
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.dinesh.quizappassignment7.R
@@ -9,9 +13,13 @@ import com.dinesh.quizappassignment7.data.Quiz
 import com.dinesh.quizappassignment7.databinding.ItemResultAnalysisBinding
 import com.dinesh.quizappassignment7.util.DiffUtilCallbackImpl
 
+
 class ResultAnalysisAdapter(
     private val quizList: MutableList<Quiz> = mutableListOf()
 ): RecyclerView.Adapter<ResultAnalysisAdapter.ResultAnalysisViewHolder>() {
+
+    private val FADE_DURATION = 1000 //FADE_DURATION in milliseconds
+
 
     inner class ResultAnalysisViewHolder(private val binding: ItemResultAnalysisBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(quiz: Quiz) {
@@ -33,6 +41,30 @@ class ResultAnalysisAdapter(
 
     override fun onBindViewHolder(holder: ResultAnalysisViewHolder, position: Int) {
         holder.bind(quizList[position])
+
+        // Set the view to fade in
+        setScaleAnimation(holder.itemView)
+    }
+
+    private fun setFadeAnimation(view: View) {
+        val anim = AlphaAnimation(0.0f, 1.0f)
+        anim.duration = FADE_DURATION.toLong()
+        view.startAnimation(anim)
+    }
+
+    private fun setScaleAnimation(view: View) {
+        val anim = ScaleAnimation(
+            0.0f,
+            1.0f,
+            0.0f,
+            1.0f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f
+        )
+        anim.duration = FADE_DURATION.toLong()
+        view.startAnimation(anim)
     }
 
     override fun getItemCount(): Int = quizList.size
